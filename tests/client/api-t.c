1d4c41a95a1b9738862f7ec373172286d69f1811
Russ Allbery <rra@stanford.edu>
2006-07-27 01:08:32 +0000

    Test both protocol v1 and protocol v2.

--- tests/client/api-t.c
+++ tests/client/api-t.c
@@ -13,6 +13,7 @@
 #include <sys/wait.h>
 
 #include <client/remctl.h>
+#include <client/internal.h>
 #include <tests/libtest.h>
 #include <util/util.h>
 
@@ -47,79 +48,101 @@ spawn_remctl(char *principal)
     }
 }
 
+/* Takes the current test number, the principal, and the protocol version and
+   runs a set of tests.  Due to the compatibility layer, we should be able to
+   run the same commands regardless of the protocol (we're not testing any of
+   the v2-specific features here).  Returns the next test number. */
+static int
+do_tests(int n, const char *principal, int protocol)
+{
+    struct remctl *r;
+    struct iovec *command;
+    struct remctl_output *output;
+    const char *test[] = { "test", "test", NULL };
+
+    r = remctl_new();
+    ok(n++, r != NULL);
+    ok_string(n++, "No error", remctl_error(r));
+    r->protocol = protocol;
+    ok(n++, remctl_open(r, "localhost", 14444, principal));
+    ok_string(n++, "No error", remctl_error(r));
+    ok(n++, remctl_command(r, test, 1));
+    ok_string(n++, "No error", remctl_error(r));
+    output = remctl_output(r);
+    ok(n++, output != NULL);
+    ok_int(n++, REMCTL_OUT_OUTPUT, output->type);
+    ok_int(n++, 12, output->length);
+    if (output->data == NULL)
+        ok(n++, 0);
+    else
+        ok(n++, memcmp("hello world\n", output->data, 11) == 0);
+    ok_int(n++, 1, output->stream);
+    output = remctl_output(r);
+    ok(n++, output != NULL);
+    ok_int(n++, REMCTL_OUT_STATUS, output->type);
+    ok_int(n++, 0, output->status);
+    command = xcalloc(2, sizeof(struct iovec));
+    command[0].iov_base = (char *) "test";
+    command[0].iov_len = 4;
+    command[1].iov_base = (char *) "test";
+    command[1].iov_len = 4;
+    ok(n++, remctl_commandv(r, command, 2, 1));
+    ok_string(n++, "No error", remctl_error(r));
+    output = remctl_output(r);
+    ok(n++, output != NULL);
+    ok_int(n++, REMCTL_OUT_OUTPUT, output->type);
+    ok_int(n++, 12, output->length);
+    if (output->data == NULL)
+        ok(n++, 0);
+    else
+        ok(n++, memcmp("hello world\n", output->data, 11) == 0);
+    ok_int(n++, 1, output->stream);
+    output = remctl_output(r);
+    ok(n++, output != NULL);
+    ok_int(n++, REMCTL_OUT_STATUS, output->type);
+    ok_int(n++, 0, output->status);
+    remctl_close(r);
+    ok(n++, 1);
+
+    return n;
+}
 
 int
 main(void)
 {
-    struct remctl *r;
     char *principal;
-    const char *test[] = { "test", "test", NULL };
-    struct iovec *command;
-    struct remctl_result *result;
-    struct remctl_output *output;
     pid_t remctld;
+    struct remctl_result *result;
+    const char *test[] = { "test", "test", NULL };
+    int n;
 
-    test_init(30);
+    test_init(55);
 
     principal = kerberos_setup();
     if (principal == NULL) {
-        skip_block(1, 30, "Kerberos tests not configured");
+        skip_block(1, 55, "Kerberos tests not configured");
     } else {
         remctld = spawn_remctl(principal);
         if (remctld <= 0)
             die("cannot spawn remctld");
-        r = remctl_new();
-        ok(1, r != NULL);
-        ok_string(2, "No error", remctl_error(r));
-        ok(3, remctl_open(r, "localhost", 14444, principal));
-        ok_string(4, "No error", remctl_error(r));
-        ok(5, remctl_command(r, test, 1));
-        ok_string(6, "No error", remctl_error(r));
-        output = remctl_output(r);
-        ok(7, output != NULL);
-        ok_int(8, REMCTL_OUT_OUTPUT, output->type);
-        ok_int(9, 12, output->length);
-        if (output->data == NULL)
-            ok(10, 0);
-        else
-            ok(10, memcmp("hello world\n", output->data, 11) == 0);
-        ok_int(11, 1, output->stream);
-        output = remctl_output(r);
-        ok(12, output != NULL);
-        ok_int(13, REMCTL_OUT_STATUS, output->type);
-        ok_int(14, 0, output->status);
-        command = xcalloc(2, sizeof(struct iovec));
-        command[0].iov_base = (char *) "test";
-        command[0].iov_len = 4;
-        command[1].iov_base = (char *) "test";
-        command[1].iov_len = 4;
-        ok(15, remctl_commandv(r, command, 2, 1));
-        ok_string(16, "No error", remctl_error(r));
-        output = remctl_output(r);
-        ok(17, output != NULL);
-        ok_int(18, REMCTL_OUT_OUTPUT, output->type);
-        ok_int(19, 12, output->length);
-        if (output->data == NULL)
-            ok(20, 0);
-        else
-            ok(20, memcmp("hello world\n", output->data, 11) == 0);
-        ok_int(21, 1, output->stream);
-        output = remctl_output(r);
-        ok(22, output != NULL);
-        ok_int(23, REMCTL_OUT_STATUS, output->type);
-        ok_int(24, 0, output->status);
-        remctl_close(r);
-        ok(25, 1);
 
+        n = do_tests(1, principal, 1);
+        n = do_tests(n, principal, 2);
+
+        /* We don't have a way of forcing this to use a particular protocol,
+           so we always do it via protocol v2.  But if the above worked with
+           protocol v1, and this wrapper works with v2, everything should have
+           gotten tested. */
         result = remctl("localhost", 14444, principal, test);
-        ok(26, result != NULL);
-        ok_int(27, 0, result->status);
-        ok_int(28, 12, result->stdout_len);
+        ok(n++, result != NULL);
+        ok_int(n++, 0, result->status);
+        ok_int(n++, 12, result->stdout_len);
         if (result->stdout == NULL)
-            ok(29, 0);
+            ok(n++, 0);
         else
-            ok(29, memcmp("hello world\n", result->stdout, 11) == 0);
-        ok(30, result->error == NULL);
+            ok(n++, memcmp("hello world\n", result->stdout, 11) == 0);
+        ok(n++, result->error == NULL);
+
         kill(remctld, SIGTERM);
         waitpid(remctld, NULL, 0);
     }
