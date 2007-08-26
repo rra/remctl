736baf901305afad3815de24d9bf60d27b8f14ba
Russ Allbery <rra@stanford.edu>
2007-08-26 02:23:43 +0000

    Include a dummy symbol in libportable so that it always contains at least one object.  Fixes compilation problems on Mac OS X 10.4 and Solaris 10.

---
+++ portable/dummy.c
@@ -0,0 +1,18 @@
+/*  $Id$
+**
+**  Dummy symbol to prevent an empty library.
+**
+**  Written by Russ Allbery <rra@stanford.edu>
+**  This work is hereby placed in the public domain by its author.
+**
+**  On platforms that already have all of the functions that libportable would
+**  supply, Automake builds an empty library and then calls ar with
+**  nonsensical arguments.  Ensure that libportable always contains at least
+**  one symbol.
+*/
+
+int
+portable_dummy(void)
+{
+    return 42;
+}
