b9321899dfe040fae80a52f9f1db9e39e8d00efd
Russ Allbery <rra@stanford.edu>
2008-03-30 23:12:42 +0000

    Also test that all file descriptors above 2 are closed.

--- tests/data/cmd-closed.c
+++ tests/data/cmd-closed.c
@@ -1,7 +1,7 @@
 /*  $Id$
 **
 **  Small C program to verify that standard input is not closed but returns
-**  EOF on any read.
+**  EOF on any read and that all file descriptors higher than 2 are closed.
 */
 
 #include <config.h>
@@ -14,16 +14,26 @@ main(void)
 {
     char buffer;
     ssize_t count;
+    int i;
 
+    /* First check that standard input is not closed but returns EOF. */
     count = read(0, &buffer, 1);
-    if (count == 0) {
-        printf("Okay");
-        exit(0);
-    } else if (count > 0) {
+    if (count > 0) {
         printf("Read %d bytes\n", (int) count);
         exit(1);
-    } else {
+    } else if (count < 0) {
         printf("Failed with error: %s\n", strerror(errno));
         exit(2);
     }
+
+    /* Now, check that all higher file descriptors are closed.  (We only go up
+       to 31; it's very unlikely that there will be problems higher than
+       that.) */
+    for (i = 3; i < 32; i++)
+        if (close(i) >= 0 || errno != EBADF) {
+            printf("File descriptor %d was open\n", i);
+            exit(3);
+        }
+    printf("Okay");
+    exit(0);
 }
