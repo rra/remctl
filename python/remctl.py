a0de95c7d720bf08d31dcbeee609e014d63c822c
Russ Allbery <rra@stanford.edu>
2008-08-20 18:43:19 -0700

    Return the error string from exceptions without quotes
    Use str() instead of repr() to format the argument to a remctl exception
    when asked for the string version.  repr() adds extra enclosing single
    quotes that aren't useful and don't match other remctl interfaces.

--- python/remctl.py
+++ python/remctl.py
@@ -24,7 +24,7 @@ class Error(Exception):
     def __init__(self, value):
 	self.value = value
     def __str__(self):
-	return repr(self.value)
+	return str(self.value)
 
 class RemctlArgError(Error):
     """Invalid arguments supplied."""
