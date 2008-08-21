68d9dc72b82478c2d68f1255e69e539c09d2d233
Russ Allbery <rra@stanford.edu>
2008-08-20 18:44:12 -0700

    Add additional Python tests
    Test remctl protocol errors from the simple interface and test success
    and protocol errors from the full interface.

--- python/test_remctl.py
+++ python/test_remctl.py
@@ -17,11 +17,13 @@ class TestRemctl(unittest.TestCase):
         elif os.path.isfile('../../tests/data/conf-simple'):
             os.chdir('../../tests')
         assert(os.path.isfile('data/conf-simple'))
+
     def get_principal(self):
         file = open('data/test.principal', 'r')
         principal = file.read().rstrip()
         file.close()
         return principal
+
     def start_remctld(self):
         try:
             os.remove('data/pid')
@@ -35,6 +37,7 @@ class TestRemctl(unittest.TestCase):
                      '-s', principal, '-P', 'data/pid', '-f',
                      'data/conf-simple', '-d', '-S', '-F', '-k',
                      'data/test.keytab')
+
     def stop_remctld(self):
         try:
             file = open('data/pid', 'r')
@@ -46,6 +49,7 @@ class TestRemctl(unittest.TestCase):
         except IOError, (error, strerror):
             if error != errno.ENOENT:
                 raise
+
     def run_kinit(self):
         os.environ['KRB5CCNAME'] = 'data/test.cache'
         self.principal = self.get_principal()
@@ -59,10 +63,12 @@ class TestRemctl(unittest.TestCase):
             time.sleep(1)
         stop_remctld()
         return False
+
     def setUp(self):
         self.change_directory()
         self.start_remctld()
         assert(self.run_kinit())
+
     def tearDown(self):
         self.stop_remctld()
 
@@ -73,6 +79,36 @@ class TestRemctlSimple(TestRemctl):
         self.assertEqual(result.stdout, "hello world\n")
         self.assertEqual(result.stderr, None)
         self.assertEqual(result.status, 0)
+
+    def test_simple_failure(self):
+        command = ('test', 'bad-command')
+        try:
+            result = remctl.remctl('localhost', 14373, self.principal, command)
+        except remctl.RemctlProtocolError, error:
+            self.assertEqual(str(error), 'Unknown command')
+
+class TestRemctlFull(TestRemctl):
+    def test_full_success(self):
+        r = remctl.Remctl()
+        r.open('localhost', 14373, self.principal)
+        r.command(['test', 'test'])
+        type, data, stream, status, error = r.output()
+        self.assertEqual(type, remctl.REMCTL_OUT_OUTPUT)
+        self.assertEqual(data, "hello world\n")
+        self.assertEqual(stream, 1)
+        type, data, stream, status, error = r.output()
+        self.assertEqual(type, remctl.REMCTL_OUT_STATUS)
+        self.assertEqual(status, 0)
+        type, data, stream, status, error = r.output()
+        self.assertEqual(type, remctl.REMCTL_OUT_DONE)
+
+    def test_full_failure(self):
+        r = remctl.Remctl('localhost', 14373, self.principal)
+        r.command(['test', 'bad-command'])
+        type, data, stream, status, error = r.output()
+        self.assertEqual(type, remctl.REMCTL_OUT_ERROR)
+        self.assertEqual(data, 'Unknown command')
+        self.assertEqual(error, 5)
 
 if __name__ == '__main__':
     unittest.main()
