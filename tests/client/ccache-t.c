46348c839464aeffd0a9350112ebf96d71d3d46f
Russ Allbery <rra@stanford.edu>
2011-09-21 18:46:12 -0700

    Don't fail the ccache test if GSS-API doesn't support it
    Skip the test if GSS-API doesn't support setting the Kerberos
    credential cache.

--- tests/client/ccache-t.c
+++ tests/client/ccache-t.c
@@ -28,6 +28,7 @@ main(void)
     pid_t remctld;
     struct remctl *r;
     struct remctl_output *output;
+    int status;
     const char *command[] = { "test", "test", NULL };
 
     if (chdir(getenv("SOURCE")) < 0)
@@ -53,27 +54,35 @@ main(void)
        "remctl_open to 127.0.0.1");
 
     /* Set the ticket cache and connect to 127.0.0.1 and run a command. */
-    ok(remctl_set_ccache(r, cache), "remctl_set_ccache");
-    ok(remctl_open(r, "127.0.0.1", 14373, principal),
-       "remctl_open to 127.0.0.1");
-    ok(remctl_command(r, command), "remctl_command");
-    output = remctl_output(r);
-    ok(output != NULL, "remctl_output #1");
-    if (output == NULL)
-        ok_block(3, 0, "remctl_output failed");
-    else {
-        is_int(REMCTL_OUT_OUTPUT, output->type, "output token");
-        ok(memcmp("hello world\n", output->data, strlen("hello world\n")) == 0,
-           "correct output");
-        is_int(strlen("hello world\n"), output->length, "correct length");
-    }
-    output = remctl_output(r);
-    ok(output != NULL, "remctl_output #2");
-    if (output == NULL)
-        ok_block(2, 0, "remctl_output failed");
-    else {
-        is_int(REMCTL_OUT_STATUS, output->type, "status token");
-        is_int(0, output->status, "status is correct");
+    status = remctl_set_ccache(r, cache);
+    if (!status) {
+        is_string("setting credential cache not supported", remctl_error(r),
+                  "remctl_set_ccache failed with correct error");
+        skip_block(8, "credential cache setting not supported");
+    } else {
+        ok(remctl_set_ccache(r, cache), "remctl_set_ccache");
+        ok(remctl_open(r, "127.0.0.1", 14373, principal),
+           "remctl_open to 127.0.0.1");
+        ok(remctl_command(r, command), "remctl_command");
+        output = remctl_output(r);
+        ok(output != NULL, "remctl_output #1");
+        if (output == NULL)
+            ok_block(3, 0, "remctl_output failed");
+        else {
+            is_int(REMCTL_OUT_OUTPUT, output->type, "output token");
+            ok(memcmp("hello world\n", output->data,
+                      strlen("hello world\n")) == 0,
+               "correct output");
+            is_int(strlen("hello world\n"), output->length, "correct length");
+        }
+        output = remctl_output(r);
+        ok(output != NULL, "remctl_output #2");
+        if (output == NULL)
+            ok_block(2, 0, "remctl_output failed");
+        else {
+            is_int(REMCTL_OUT_STATUS, output->type, "status token");
+            is_int(0, output->status, "status is correct");
+        }
     }
     remctl_close(r);
 
