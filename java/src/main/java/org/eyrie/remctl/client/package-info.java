d37d85e4e04780b950bac645223d336cb6fdb87a
Patrick Radtke <pradtke@stanford.edu>
2011-10-27 17:03:19 -0700

    Add package docs

---
+++ java/src/main/java/org/eyrie/remctl/client/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides classes used by remctl clients.
+ */
+package org.eyrie.remctl.client;
+
---
+++ java/src/main/java/org/eyrie/remctl/core/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides common core remctl classes that can be used by both client and servers.
+ */
+package org.eyrie.remctl.core;
+
