3348ed1c4efa61f803aee39b20e769a7efe9bc87
Russ Allbery <eagle@eyrie.org>
2014-01-26 23:40:11 -0800

    Add missing END_DECLS to portable/krb5.h
    This would have caused compilation errors with a C++ compiler.
    
    Change-Id: Ie3cf92bb0bbdbf172c72ab6be1b3cf64f58639d5
    Reviewed-on: https://gerrit.stanford.edu/1412
    Reviewed-by: Russ Allbery <rra@stanford.edu>
    Tested-by: Russ Allbery <rra@stanford.edu>

--- portable/krb5.h
+++ portable/krb5.h
@@ -98,6 +98,8 @@ void krb5_free_error_message(krb5_context, const char *);
 const char *krb5_principal_get_realm(krb5_context, krb5_const_principal);
 #endif
 
+END_DECLS
+
 /* Undo default visibility change. */
 #pragma GCC visibility pop
 
