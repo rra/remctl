30806e4c8aa209ca892fed558078a9e5b09eed52
Russ Allbery <eagle@eyrie.org>
2020-12-13 13:36:59 -0800

    Add more rra-c-util 8.4 updates
    * Fix reallocarray prototyping on NetBSD.
    * Fix getnameinfo tests on musl-based Linux distributions.

--- portable/system.h
+++ portable/system.h
@@ -139,7 +139,7 @@ extern int daemon(int, int);
 #if !HAVE_MKSTEMP
 extern int mkstemp(char *);
 #endif
-#if !HAVE_REALLOCARRAY
+#if !HAVE_DECL_REALLOCARRAY
 extern void *reallocarray(void *, size_t, size_t);
 #endif
 #if !HAVE_SETENV
